38a78856dc2b0dde642a0fa79ebde0167f5ca827
Marvin Herbold <521887+mherbold@users.noreply.github.com>
2025-07-05 16:25:14 -0700

    Add update interval and counter

--- Components/Debug.cs
+++ Components/Debug.cs
@@ -5,6 +5,8 @@ namespace MarvinsAIRARefactored.Components;
 
 public class Debug
 {
+	private const int UpdateInterval = 6;
+
 	public string Label_1 { private get; set; } = string.Empty;
 	public string Label_2 { private get; set; } = string.Empty;
 	public string Label_3 { private get; set; } = string.Empty;
@@ -16,21 +18,30 @@ public class Debug
 	public string Label_9 { private get; set; } = string.Empty;
 	public string Label_10 { private get; set; } = string.Empty;
 
+	private int _updateCounter = UpdateInterval + 1;
+
 	[MethodImpl( MethodImplOptions.AggressiveInlining )]
 	public void Tick( App app )
 	{
-		if ( app.MainWindow.DebugTabItemIsVisible )
+		_updateCounter--;
+
+		if ( _updateCounter == 0 )
 		{
-			app.MainWindow.Debug_Label_1.Content = Label_1;
-			app.MainWindow.Debug_Label_2.Content = Label_2;
-			app.MainWindow.Debug_Label_3.Content = Label_3;
-			app.MainWindow.Debug_Label_4.Content = Label_4;
-			app.MainWindow.Debug_Label_5.Content = Label_5;
-			app.MainWindow.Debug_Label_6.Content = Label_6;
-			app.MainWindow.Debug_Label_7.Content = Label_7;
-			app.MainWindow.Debug_Label_8.Content = Label_8;
-			app.MainWindow.Debug_Label_9.Content = Label_9;
-			app.MainWindow.Debug_Label_10.Content = Label_10;
+			_updateCounter = UpdateInterval;
+
+			if ( app.MainWindow.DebugTabItemIsVisible )
+			{
+				app.MainWindow.Debug_Label_1.Content = Label_1;
+				app.MainWindow.Debug_Label_2.Content = Label_2;
+				app.MainWindow.Debug_Label_3.Content = Label_3;
+				app.MainWindow.Debug_Label_4.Content = Label_4;
+				app.MainWindow.Debug_Label_5.Content = Label_5;
+				app.MainWindow.Debug_Label_6.Content = Label_6;
+				app.MainWindow.Debug_Label_7.Content = Label_7;
+				app.MainWindow.Debug_Label_8.Content = Label_8;
+				app.MainWindow.Debug_Label_9.Content = Label_9;
+				app.MainWindow.Debug_Label_10.Content = Label_10;
+			}
 		}
 	}
 }
