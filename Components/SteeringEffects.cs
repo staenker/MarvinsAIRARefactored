cf589203e128434e30d10112b5ad654e4f92421c
Marvin Herbold <521887+mherbold@users.noreply.github.com>
2025-07-07 18:01:18 -0700

    Updated steering effect calibration code

--- Components/SteeringEffects.cs
+++ Components/SteeringEffects.cs
@@ -1,53 +1,69 @@
 ﻿
-using System.Globalization;
 using System.IO;
-
-using CsvHelper;
 
 namespace MarvinsAIRARefactored.Components;
 
 public class SteeringEffects
 {
-	private class RecordingData
-	{
-		public float SteeringWheelAngle { get; set; }
-		public float SpeedInKPH { get; set; }
-		public float YawRate { get; set; }
-	}
+	private const float RadiansToDegrees = 180f / MathF.PI;
 
-	private const float KPHToMetersPerSecond = 1f / 3.6f;
+	private const int SteeringWheelAngleInterval = 30;
+	private const int MaxSteeringWheelAngle = 180;
+	private const int MaxSpeedInMPS = 90;
+	private const int NumSteeringWheelAngles = MaxSteeringWheelAngle * 2 / SteeringWheelAngleInterval + 1;
 
 	private readonly string _recordingsDirectory = Path.Combine( App.DocumentsFolder, "Recordings" );
 
 	private bool _isCalibrating = false;
 	private bool _isStopping = false;
-	private int _targetSpeedInKPH = 0;
-	private float _lastFrameVelocityX = 0f;
 
-	private readonly RecordingData[] _recordingData = new RecordingData[ 300 ];
+	private int _initialSteeringWheelAngle = 0;
+
+	private int _targetSteeringWheelAngle = 0;
+	private int _targetSpeedInMPS = 0;
+	private float _targetThrottle = 0f;
+
+	private float _lastFrameVelocityX = 0f;
+	private float _throttleFade = 0f;
+	private float _maxYawRate = 0f;
+	private float _settleTimer = 0f;
+
+	private int _numSteeringWheelAngles = 0;
+
+	private readonly float[,] _yawRateData = new float[ NumSteeringWheelAngles, MaxSpeedInMPS + 1 ];
 
 	public void RunCalibration()
 	{
 		var app = App.Instance!;
 
-		// shortcut to settings
-
-		var settings = DataContext.DataContext.Instance.Settings;
-
-		// round the target steering wheel angle to the nearest degree
-
-		var steeringWheelAngle = MathF.Round( settings.SteeringEffectsSteeringWheelAngle );
-
 		// set initial virtual joystick parameters
 
-		app.VirtualJoystick.SteeringWheelAngle = steeringWheelAngle / 450f;
+		app.VirtualJoystick.SteeringWheelAngle = 0f;
 		app.VirtualJoystick.Throttle = 0f;
 		app.VirtualJoystick.Brake = 0f;
 
-		// set target speed
+		// set initial targets
 
-		_targetSpeedInKPH = 1;
+		_initialSteeringWheelAngle = (int) -MathF.Min( MaxSteeringWheelAngle, MathF.Floor( app.Simulator.SteeringWheelAngleMax * RadiansToDegrees * 0.5f / 10 ) * 10 );
+
+		_targetSteeringWheelAngle = _initialSteeringWheelAngle;
+		_targetSpeedInMPS = 1;
+		_targetThrottle = 0f;
+
+		// clear last frame velocity and throttle fade
+
 		_lastFrameVelocityX = 0f;
+		_throttleFade = 0f;
+		_maxYawRate = 0f;
+		_settleTimer = 0f;
+
+		// reset the number of steering wheel angles we have recorded
+
+		_numSteeringWheelAngles = 0;
+
+		// clear out our old yaw rate data
+
+		Array.Clear( _yawRateData );
 
 		// start the calibration process
 
@@ -68,97 +84,167 @@ public class SteeringEffects
 
 	public void Tick( App app )
 	{
-		// shortcut to settings
-
-		var settings = DataContext.DataContext.Instance.Settings;
-
-		float speedDelta = 0f;
-
 		if ( _isCalibrating )
 		{
-			// if we aren't gaining speed, increase the throttle a hair
+			// give us time to settle down before starting the next pass
 
-			speedDelta = app.Simulator.VelocityX - _lastFrameVelocityX;
+			_settleTimer += 0.01f;
 
-			_lastFrameVelocityX = app.Simulator.VelocityX;
-
-			if ( speedDelta <= 0.01f )
+			if ( _settleTimer > 2f )
 			{
-				app.VirtualJoystick.Throttle += 0.0005f;
-			}
+				// figure out if we have crashed
 
-			// shift up if we are in neutral or near shift RPM
+				var crashed = app.Simulator.GForce >= 2f;
 
-			if ( ( app.Simulator.Gear == 0 ) || ( app.Simulator.RPM >= app.Simulator.ShiftLightsShiftRPM * 0.75f ) )
-			{
-				app.VirtualJoystick.ShiftUp = true;
-			}
+				// set steering wheel angle
 
-			// check if we've reached our target speed
+				app.VirtualJoystick.SteeringWheelAngle = _targetSteeringWheelAngle / 450f;
 
-			var targetSpeedInMPS = _targetSpeedInKPH * KPHToMetersPerSecond;
+				// fade in the throttle
 
-			if ( app.Simulator.VelocityX >= targetSpeedInMPS )
-			{
-				// yes - save the data
+				_throttleFade = MathF.Min( 1f, _throttleFade + 0.01f );
 
-				_recordingData[ (int) _targetSpeedInKPH ] = new RecordingData()
+				// if we aren't gaining speed, increase the throttle a hair
+
+				var speedDelta = app.Simulator.VelocityX - _lastFrameVelocityX;
+
+				_lastFrameVelocityX = app.Simulator.VelocityX;
+
+				if ( ( _throttleFade == 1f ) && ( speedDelta <= 0.005f ) )
 				{
-					SteeringWheelAngle = MathF.Round( settings.SteeringEffectsSteeringWheelAngle ),
-					SpeedInKPH = _targetSpeedInKPH,
-					YawRate = app.Simulator.YawRate
-				};
+					_targetThrottle += 0.0005f;
+				}
 
-				// bump up the target speed
+				// update the virtual joystick throttle
 
-				_targetSpeedInKPH++;
+				app.VirtualJoystick.Throttle = _targetThrottle * _throttleFade;
+
+				// shift up if we are in neutral or near shift RPM
+
+				if ( ( app.Simulator.Gear == 0 ) || ( app.Simulator.RPM >= app.Simulator.ShiftLightsShiftRPM * 0.75f ) )
+				{
+					app.VirtualJoystick.ShiftUp = true;
+
+					_throttleFade = 0f;
+				}
+
+				// check if we've reached our target speed
+
+				if ( !crashed && ( app.Simulator.VelocityX >= _targetSpeedInMPS ) )
+				{
+					// yes - save the data
+
+					_yawRateData[ _numSteeringWheelAngles, _targetSpeedInMPS ] = app.Simulator.YawRate;
+
+					_maxYawRate = MathF.Max( _maxYawRate, MathF.Abs( app.Simulator.YawRate ) );
+
+					// bump up the target speed
+
+					_targetSpeedInMPS++;
+				}
+
+				// check if we are done with this pass
+
+				if ( crashed || ( _targetSpeedInMPS > MaxSpeedInMPS ) || ( MathF.Abs( app.Simulator.VelocityY ) > 2f ) || ( MathF.Abs( app.Simulator.YawRate ) < ( _maxYawRate * 0.75f ) ) )
+				{
+					app.VirtualJoystick.SteeringWheelAngle = 0f;
+					app.VirtualJoystick.Throttle = 0f;
+					app.VirtualJoystick.Brake = 0f;
+
+					// set next targets
+
+					_targetSteeringWheelAngle += SteeringWheelAngleInterval;
+					_targetSpeedInMPS = 1;
+					_targetThrottle = 0f;
+
+					// clear last frame velocity and throttle fade
+
+					_lastFrameVelocityX = 0f;
+					_throttleFade = 0f;
+					_maxYawRate = 0f;
+					_settleTimer = 0f;
+
+					// fire active reset
+
+					app.VirtualJoystick.ActiveResetRun = true;
+
+					// increase num steering wheel angles recorded
+
+					_numSteeringWheelAngles++;
+
+					// stop calibrating when we get to the end
+
+					if ( _numSteeringWheelAngles == NumSteeringWheelAngles )
+					{
+						StopCalibration();
+					}
+				}
 			}
 		}
 		else if ( _isStopping )
 		{
+			app.VirtualJoystick.SteeringWheelAngle = 0f;
+			app.VirtualJoystick.Throttle = 0f;
 			app.VirtualJoystick.Brake = 1f;
-
-			if ( app.Simulator.Gear != 0 )
-			{
-				app.VirtualJoystick.ShiftDown = true;
-			}
 
 			if ( app.Simulator.VelocityX <= 0.001f )
 			{
 				_isStopping = false;
+
+				app.VirtualJoystick.Brake = 0f;
 			}
 		}
 
-		app.Debug.Label_1 = $"isCalibrating = {_isCalibrating}";
-		app.Debug.Label_2 = $"isStopping = {_isStopping}";
-		app.Debug.Label_3 = $"SteeringWheelAngle = {app.VirtualJoystick.SteeringWheelAngle}";
-		app.Debug.Label_4 = $"Throttle = {app.VirtualJoystick.Throttle}";
-		app.Debug.Label_5 = $"Brake = {app.VirtualJoystick.Brake}";
-		app.Debug.Label_6 = $"Gear = {app.Simulator.Gear}";
-		app.Debug.Label_7 = $"YawRate = {app.Simulator.YawRate}";
-		app.Debug.Label_8 = $"speedDelta = {speedDelta}";
-		app.Debug.Label_9 = $"targetSpeedInKPH = {_targetSpeedInKPH}";
+		app.Debug.Label_1 = $"[SteeringEffects] _isCalibrating = {_isCalibrating}, _isStopping = {_isStopping}";
+		app.Debug.Label_2 = $"[SteeringEffects] _targetSteeringWheelAngle = {_targetSteeringWheelAngle}, _targetSpeedInMPS = {_targetSpeedInMPS}, _targetThrottle = {_targetThrottle:F4}";
+		app.Debug.Label_3 = $"[SteeringEffects] _numSteeringWheelAngles = {_numSteeringWheelAngles}, _throttleFade = {_throttleFade:F2}, _maxYawRate = {_maxYawRate:F6}";
+		app.Debug.Label_4 = $"[VirtualJoystick] SteeringWheelAngle = {app.VirtualJoystick.SteeringWheelAngle:F3}, Throttle = {app.VirtualJoystick.Throttle:F3}, Brake = {app.VirtualJoystick.Brake:F3}";
+		app.Debug.Label_5 = $"[Simulator] Gear = {app.Simulator.Gear}, SteeringWheelAngle = {app.Simulator.SteeringWheelAngle * 180f / MathF.PI:F3}, YawRate = {app.Simulator.YawRate:F6}";
+		app.Debug.Label_6 = $"[Simulator] VelocityX = {app.Simulator.VelocityX:F3}, VelocityY = {app.Simulator.VelocityY:F3}";
 	}
 
 	public void SaveRecording()
 	{
-		// shortcut to settings
-
-		var settings = DataContext.DataContext.Instance.Settings;
-
 		var app = App.Instance!;
 
 		app.Logger.WriteLine( "[SteeringEffects] SaveRecording >>>" );
 
-		var filePath = Path.Combine( _recordingsDirectory, $"{app.Simulator.CarScreenName} {MathF.Round( settings.SteeringEffectsSteeringWheelAngle ):F0}.csv" );
+		// open file
+
+		var filePath = Path.Combine( _recordingsDirectory, $"{app.Simulator.CarScreenName}.csv" );
 
 		using var writer = new StreamWriter( filePath );
 
-		writer.WriteLine( "New recording" );
+		// write car name
 
-		using var csv = new CsvWriter( writer, CultureInfo.InvariantCulture );
+		writer.WriteLine( app.Simulator.CarScreenName );
 
-		csv.WriteRecords( _recordingData );
+		// write header row
+
+		var steeringWheelAngle = _initialSteeringWheelAngle;
+
+		var headerString = "Speed";
+
+		for ( var i = 0; i < _numSteeringWheelAngles; i++ )
+		{
+			headerString += $",{steeringWheelAngle + i * SteeringWheelAngleInterval}";
+		}
+
+		writer.WriteLine( headerString );
+
+		// write data rows
+
+		for ( var j = 0; j <= MaxSpeedInMPS; j++ )
+		{
+			var dataString = $"{j}";
+
+			for ( var i = 0; i < _numSteeringWheelAngles; i++ )
+			{
+				dataString += $",{_yawRateData[ i, j ]:F6}";
+			}
+
+			writer.WriteLine( dataString );
+		}
 
 		app.Logger.WriteLine( "[SteeringEffects] <<< SaveRecording" );
 	}
