dab5cea8c88330be6ff533d84d87cb9a3d5adaa5
Marvin Herbold <marvin.herbold@gmail.com>
2025-09-22 08:04:20 -0700

    New text box user control

---
+++ Classes/TextBoxBehaviors.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+using DataFormats = System.Windows.DataFormats;
+using DataObject = System.Windows.DataObject;
+using TextBox = System.Windows.Controls.TextBox;
+
+namespace MarvinsAIRARefactored.Classes;
+
+public static class TextBoxBehaviors
+{
+	public static readonly DependencyProperty IsNumericOnlyProperty = DependencyProperty.RegisterAttached( "IsNumericOnly", typeof( bool ), typeof( TextBoxBehaviors ), new PropertyMetadata( false, OnIsNumericOnlyChanged ) );
+
+	public static bool GetIsNumericOnly( TextBox textBox ) => (bool) textBox.GetValue( IsNumericOnlyProperty );
+	public static void SetIsNumericOnly( TextBox textBox, bool value ) => textBox.SetValue( IsNumericOnlyProperty, value );
+
+	private static void OnIsNumericOnlyChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
+	{
+		if ( d is TextBox textBox )
+		{
+			if ( (bool) e.NewValue )
+			{
+				textBox.PreviewKeyDown += TextBox_PreviewKeyDown;
+				textBox.PreviewTextInput += TextBox_PreviewTextInput;
+
+				DataObject.AddPastingHandler( textBox, OnPaste );
+			}
+			else
+			{
+				textBox.PreviewKeyDown -= TextBox_PreviewKeyDown;
+				textBox.PreviewTextInput -= TextBox_PreviewTextInput;
+
+				DataObject.RemovePastingHandler( textBox, OnPaste );
+			}
+		}
+	}
+
+	private static void TextBox_PreviewKeyDown( object sender, System.Windows.Input.KeyEventArgs e )
+	{
+		if ( e.Key == Key.Space )
+		{
+			e.Handled = true;
+
+			return;
+		}
+
+		switch ( e.Key )
+		{
+			case Key.Back:
+			case Key.Delete:
+			case Key.Tab:
+			case Key.Left:
+			case Key.Right:
+			case Key.Home:
+			case Key.End:
+			{
+				e.Handled = false;
+				break;
+			}
+		}
+	}
+
+	private static void TextBox_PreviewTextInput( object sender, TextCompositionEventArgs e )
+	{
+		if ( sender is TextBox textBox )
+		{
+			e.Handled = !IsInsertionNumeric( textBox, e.Text );
+		}
+	}
+
+	private static void OnPaste( object sender, DataObjectPastingEventArgs e )
+	{
+		if ( sender is not TextBox textBox )
+		{
+			e.CancelCommand();
+
+			return;
+		}
+
+		if ( e.DataObject.GetDataPresent( DataFormats.Text ) )
+		{
+			var pastedText = e.DataObject.GetData( DataFormats.Text ) as string ?? string.Empty;
+
+			if ( !IsInsertionNumeric( textBox, pastedText ) )
+			{
+				e.CancelCommand();
+			}
+		}
+		else
+		{
+			e.CancelCommand();
+		}
+	}
+
+	private static bool IsInsertionNumeric( TextBox textBox, string input )
+	{
+		if ( string.IsNullOrEmpty( input ) ) return false;
+		if ( input.Any( char.IsWhiteSpace ) ) return false;
+
+		var selectionStart = textBox.SelectionStart;
+		var selectionLength = textBox.SelectionLength;
+
+		var current = textBox.Text ?? string.Empty;
+		var before = current[ ..selectionStart ];
+		var after = current[ ( selectionStart + selectionLength ).. ];
+
+		var proposed = before + input + after;
+
+		if ( proposed.Length == 0 ) return false;
+
+		if ( proposed.Any( c => !char.IsDigit( c ) ) ) return false;
+
+		if ( proposed.Length > 1 && proposed.StartsWith( '0' ) ) return false;
+
+		return int.TryParse( proposed, out _ );
+	}
+}
---
+++ Controls/MairaTextBox.xaml.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+using UserControl = System.Windows.Controls.UserControl;
+
+namespace MarvinsAIRARefactored.Controls;
+
+public partial class MairaTextBox : UserControl
+{
+	public MairaTextBox()
+	{
+		InitializeComponent();
+	}
+
+	#region User Control Events
+
+	private void InnerTextBox_KeyDown( object sender, System.Windows.Input.KeyEventArgs e )
+	{
+		if ( e.Key == Key.Enter )
+		{
+			// Force binding update (in case UpdateSourceTrigger isn’t PropertyChanged)
+			var textBox = (TextBox) sender;
+			var binding = textBox.GetBindingExpression( TextBox.TextProperty );
+			binding?.UpdateSource();
+
+			// Remove focus from the TextBox
+			textBox.MoveFocus( new TraversalRequest( FocusNavigationDirection.Next ) );
+
+			e.Handled = true; // stop ding sound
+		}
+	}
+
+	#endregion
+
+	#region Dependency Properties
+
+	public static readonly DependencyProperty LabelProperty = DependencyProperty.Register( nameof( Label ), typeof( string ), typeof( MairaTextBox ), new PropertyMetadata( string.Empty ) );
+
+	public string Label
+	{
+		get => (string) GetValue( LabelProperty );
+		set => SetValue( LabelProperty, value );
+	}
+
+	public static readonly DependencyProperty ValueProperty = DependencyProperty.Register( nameof( Value ), typeof( string ), typeof( MairaTextBox ), new PropertyMetadata( string.Empty ) );
+
+	public string Value
+	{
+		get => (string) GetValue( ValueProperty );
+		set => SetValue( ValueProperty, value );
+	}
+
+	public static readonly DependencyProperty IsNumericOnlyProperty = DependencyProperty.Register( nameof( IsNumericOnly ), typeof( bool), typeof( MairaTextBox ) );
+
+	public bool IsNumericOnly
+	{
+		get => (bool) GetValue( ValueProperty );
+		set => SetValue( ValueProperty, value );
+	}
+
+	#endregion
+
+	#region Dependency Property Changed Events
+
+	#endregion
+
+	#region Logic
+
+	#endregion
+}
