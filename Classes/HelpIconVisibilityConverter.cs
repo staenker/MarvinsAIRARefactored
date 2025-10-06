27775ae1def391456cd9a9cceada5e8f846ce0cb
Marvin Herbold <marvin.herbold@gmail.com>
2025-10-06 16:30:44 -0700

    Make help icon visible only for elements that have a help topic

---
+++ Classes/HelpIconVisibilityConverter.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MarvinsAIRARefactored.Classes;
+
+public sealed class HelpIconVisibilityConverter : IMultiValueConverter
+{
+	public object? Convert( object[] values, Type targetType, object? parameter, CultureInfo culture )
+	{
+		var isOver = values.Length > 0 && values[ 0 ] is bool b && b;
+		var topic = values.Length > 1 ? values[ 1 ] as string : null;
+
+		return ( isOver && !string.IsNullOrWhiteSpace( topic ) ) ? Visibility.Visible : Visibility.Collapsed;
+	}
+
+	public object[] ConvertBack( object value, Type[] targetTypes, object? parameter, CultureInfo culture ) => throw new NotSupportedException();
+}
