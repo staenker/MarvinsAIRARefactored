938e7dfa0bec4c6e265b50dde90e235128f97036
Marvin Herbold <marvin.herbold@gmail.com>
2026-03-10 13:36:35 -0700

    Add second function

--- Converters/BooleanToVisibilityCollapsedConverter.cs
+++ Converters/BooleanToVisibilityCollapsedConverter.cs
@@ -28,3 +28,27 @@ public class BooleanToVisibilityCollapsedConverter : IValueConverter
 		return DependencyProperty.UnsetValue;
 	}
 }
+
+[ValueConversion( typeof( bool ), typeof( Visibility ) )]
+public class BooleanToVisibilityHiddenConverter : IValueConverter
+{
+	public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		if ( value is bool b && b )
+		{
+			return Visibility.Visible;
+		}
+
+		return Visibility.Hidden;
+	}
+
+	public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+	{
+		if ( value is Visibility v )
+		{
+			return v == Visibility.Visible;
+		}
+
+		return DependencyProperty.UnsetValue;
+	}
+}
